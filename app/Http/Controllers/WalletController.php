bb8587d299ecf9c64ed5f64c02d6c62abe20fc1d
Jeison Pedroso <jeison.contas@gmail.com>
2020-11-27 01:02:57 -0300

    wallet controller com funcao de deposito com a classe exigindo autenticação

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlcaponexD
+ * Date: 26/11/2020
+ * Time: 23:59
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Jobs\SendMail;
+use App\Rules\AmountIsPositive;
+use App\Services\WalletService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Ramsey\Uuid\Type\Decimal;
+
+class WalletController extends Controller
+{
+    private $wallet;
+
+    /**
+     * WalletController constructor.
+     * @param WalletService $walletService
+     */
+    public function __construct(WalletService $walletService)
+    {
+        $this->wallet = $walletService;
+        $this->middleware('auth');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function deposit(Request $request)
+    {
+        $this->validate($request,[
+           'amount' => ['required','integer',new AmountIsPositive]
+        ]);
+
+        $deposit = $this->wallet->deposit($request->all());
+        return response()->json($deposit);
+    }
+}
