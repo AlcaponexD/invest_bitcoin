05f1d079c1c4815dd5b806231725e05a85531707
Jeison Pedroso <jeison.contas@gmail.com>
2020-11-27 01:00:04 -0300

    migration de wallets

---
+++ database/migrations/2020_11_27_013914_create_table_wallets.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableWallets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            /**
+             * Need many lenght to encrypted data
+             */
+            $table->string('brl_amount',1000);
+            $table->string('btc_amount',1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
